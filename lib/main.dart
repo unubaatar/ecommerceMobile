15336cce92380d7b79201082dbf22383497d14ef
nukkueno <unukubodiamar@gmail.com>
2025-01-29 00:59:37 +0800

    login and cart drawer added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/home.dart';
 import 'screens/initial.dart';
-import 'screens/productDetail.dart';
 import 'screens/products.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -14,19 +17,107 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final baseUrl = 'http://13.231.156.66/api';
   int _currentIndex = 0;
-
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   final List<Widget> _screens = [
-    Home(),    
-    Initial(),        
-    Products(),      
+    Home(),
+    Initial(),
   ];
 
   void _onItemTap(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
+    if (index == 3) {
+      _showLoginDialog();
+    } else if(index == 2) {
+      _showCart();
+    } else {
+      setState(() {
+        _currentIndex = index;
+      });
+    }
+  }
+
+  Future<void> login() async {
+    try {
+      final body = {
+        "phone": phoneController.text,
+        "password": passwordController.text
+      };
+      final header = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer YOUR_API_KEY",
+      };
+      final Uri url = Uri.parse('$baseUrl/customers/login');
+      final response =
+          await http.post(url, headers: header, body: jsonEncode(body));
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body);
+        final customerId = jsonData['customer'];
+        saveCustomerId(customerId);
+        phoneController.text = '';
+        passwordController.text = '';
+        print(getCustomerId());
+        Navigator.pop(context); 
+      } else {
+        print(response.statusCode);
+      }
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  Future<void> saveCustomerId(String customerId) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('customerId', customerId);
+  }
+
+  Future<String?> getCustomerId() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('customerId');
+  }
+
+  void _showCart() {
+    _scaffoldKey.currentState?.openEndDrawer();
+  }
+
+  void _showLoginDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Login'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: phoneController,
+                  decoration: const InputDecoration(labelText: 'Phone'),
+                ),
+                TextField(
+                    decoration: const InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    controller: passwordController)
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  login();
+                },
+                child: const Text('Login'),
+              ),
+            ],
+          );
+        });
   }
 
   @override
@@ -37,33 +128,58 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
+        key: _scaffoldKey,
         appBar: AppBar(
           title: Text("Ecommerce"),
           centerTitle: true,
           backgroundColor: Colors.blue,
           foregroundColor: Colors.white,
         ),
-        body: _screens[_currentIndex], 
-        bottomNavigationBar: BottomNavigationBar( 
-          selectedItemColor: Colors.black, 
-          unselectedItemColor: Colors.grey,  
-          currentIndex: _currentIndex,  
-          onTap: _onItemTap,  
+        body: _screens[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey,
+          currentIndex: _currentIndex,
+          onTap: _onItemTap,
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
-              label: 'Home', 
+              label: 'Home',
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.search),
-              label: 'Search', 
+              label: 'Search',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.list),
-              label: 'Products',  
+              icon: Icon(Icons.add_shopping_cart),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.login),
+              label: 'Login',
+            ),
+
+          ],
+        ),
+        endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            ListTile(
+              title: Text("Home"),
+            ),
+            ListTile(
+              title: Text("Search"),
+            ),
+            ListTile(
+              title: Text("Profile"),
+            ),
+            ListTile(
+              title: Text("Settings"),
             ),
           ],
         ),
+      ),
       ),
     );
   }
